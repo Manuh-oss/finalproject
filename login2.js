698a53a61acfe0ee3a894b316c0e494c246e0757
emmanuel <emmanuelmwendwa213@gmail.com>
2025-07-23 12:31:15 +0300

    intial commit

---
+++ login2.js
@@ -0,0 +1,6 @@
+const loginBox = document.querySelector(".login");
+const otpBox = document.querySelector(".verify");
+
+document.addEventListener("DOMContentLoaded" , () => {
+    loginBox.classList.add("active");
+})
